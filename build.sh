570af28989f9f086d1f1030b28a371ef9427027f
psuwala <oneat@vp.pl>
2021-12-29 21:34:47 +0100

    fixed inplace

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-find _notes/external/mindoo/ -name "*.md" -exec sed -iE "s/$/  /g" {} \;
+find _notes/external/mindoo/ -name "*.md" -exec sed -i "s/$/  /g" {} \;
 jekyll build --trace
